897fc1dd29c163c0e80e23c64a01b20099b9caab
schwancr <schwancr@stanford.edu>
2014-05-01 13:58:09 -0700

    dunno

--- klearn/version.py
+++ klearn/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM KLEARN SETUP.PY
 short_version = '0.1'
 version = '0.1'
-full_version = '0.1.dev-57c4abf'
-git_revision = '57c4abf7c3c1ab0fc90aba6270bba27bef936e6d'
+full_version = '0.1.dev-cc03c66'
+git_revision = 'cc03c66e09c199b4da80681486b220eb0e8282cc'
 release = False
 
 if not release:
